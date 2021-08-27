e569937a9e9052b78e5c2156c9de80d33affbb70
nahomz <nahomzelalem11@gmail.com>
2021-08-27 03:51:42 -0700

    hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(viod)
+{
+
+        puts("\"proramming is like building a multilingual puzzle");
+        return (0);
+
+}
+  
