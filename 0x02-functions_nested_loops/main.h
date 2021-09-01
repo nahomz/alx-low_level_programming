2bdc84ee830d08cd54b46880a6b790593528e2b6
nahomz <nahomzelalem11@gmail.com>
2021-09-01 02:48:57 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,16 @@
+#ifndef main.h
+#define main.h
 int _putchar(char c);
 void print_alphabet(void)
 void print_alphabet_x10(void)
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_baver(void);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
 void time_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
