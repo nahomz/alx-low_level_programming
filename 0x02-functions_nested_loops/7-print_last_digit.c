b02e97e3c94bd6013159a65ffd019e95692b6be4
nahomz <nahomzelalem11@gmail.com>
2021-09-02 15:10:53 -0700

    functions_nasted_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,20 @@
 #include "main.h"
 /**
+* _abs - returns 1 if n is lowercase, 0 otherwise
+* @n: integer whose is to be printed
+* Return: abs(n)
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-1 * n);
+}
+/**
 * print_last_digit - prints the last digit of a number
-* @n: integer whose last digit is to be printed
-* Return: lastdigit of n
+* @n: integer to be tested
+* Return: lastDigit of n
 */
 int print_last_digit(int n)
 {
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ continue;
 }
 prod = i * j;
 _putchar(',');
-_putchar('.');
+_putchar(' ');
 if (prod >= 10)
 {
 _putchar(prod / 10 + 48);
