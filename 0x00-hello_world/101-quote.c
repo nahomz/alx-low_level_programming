805f34020a09472b65e8741a279e4d82fa9f295a
nahomz <nahomzelalem11@gmail.com>
2021-09-04 06:54:13 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
+* main - outputs to string
 *
-* Return: Always 0 (Success)
+* Description: Prints out "and that piece of art is useful..." without printf
+*
+* Return: 1
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dore Korpar, 2015-10-19\n", 61);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
 return (1);
 }
