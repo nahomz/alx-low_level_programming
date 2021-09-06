b4aebe70067896d237fa602260a0d6afca8d777b
nahomz <nahomzelalem11@gmail.com>
2021-09-06 01:42:44 -0700

    pointers

---
+++ 0x05-ponters_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* swap_int - swaps the value of two integers
+* @a: pointer a
+* @b: pointer b
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
---
+++ 0x05-ponters_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05-pointers_arrays_strings in this directory projects work on alx program
