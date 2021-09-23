093754274dc520330c80a6dc56c84d1ca8bed9c0
nahomz <nahomzelalem11@gmail.com>
2021-09-23 14:19:17 -0700

    update more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 /**
-* malloc_checked - allocates memory using malloc
-* @b: bytes allocate
-* Return: pointer
+*malloc_checked - unction that allocates memory using malloc.
+*@b: unsigned int
+*Return: VOID Pointer
 */
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
-ptr = malloc(b);
-if (ptr == NULL)
+/* We don't know the type of bytes that will*/
+/*occupy the allocated memory so we use VOID pointer type */
+void *pointer;
+/* Dynamic memory allocation using malloc function */
+pointer = malloc(b);
+/* if malloc fails to allocate the needed memory */
+if (pointer == NULL)
+/* exit status value */
 exit(98);
-return (ptr);
+/* the function return a pointer */
+return (pointer);
 }
