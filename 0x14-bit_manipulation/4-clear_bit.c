f27e0fdfda80aeaa10359670e98feadaf25aac9b
nahomz <nahomzelalem11@gmail.com>
2021-10-14 15:17:10 -0700

    bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,39 +1,33 @@
-#include  "main.h"
+#include "main.h"
+#define  BIT_SIZE 8
 /**
-*getp - returns the value of x to the power of y.
-*@x: number.
-*@y: power.
-*
-*Return: x to the power of y.
+* powX - powers a number b to the p's power
+* @b: base
+* @p: power
+* Return: return b to the power of a
 */
-int getp(int x, int y)
+unsigned long int powX(int b, int p)
 {
-if (y < 0)
-return (-1);
-if (y == 0)
-return (1);
-if (y == 1)
-return (x);
-return (x * getp(x, y - 1));
+unsigned long int ans = 1;
+while (p)
+{
+ans *= b;
+p--;
+}
+return (ans);
 }
 /**
-*clear_bit - stes the value of a bit to one at a given index.
-*@n: pointer to number.
-*@index: index of bit.
-*
-*Return: 1 if it worked, -1 if it did not.
+* clear_bit - sets bit to zero at index index
+* @n: input integer
+* @index: returns the value of a bit at a given index
+* Return: 1 for ssucess -1 for failure
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index > 63)
+unsigned long int test;
+if (index > sizeof(n) * BIT_SIZE - 1)
 return (-1);
-if (n)
-{
-*n -= getp(2, index);
+test = powX(2, index);
+*n = (*n & test) ? *n ^ test : *n;
 return (1);
-}
-else
-{
-return (-1);
-}
 }
