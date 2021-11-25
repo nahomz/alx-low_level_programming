f0d58e30a2d2c7e95d4f7ab42deae3573bbf5338
nahomz <nahomzelalem11@gmail.com>
2021-11-25 20:50:02 +0000

    intial commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,36 +1,37 @@
 #include "lists.h"
 
 /**
- *add_dnodeint_end-Adds a new node at the end of a double linked
- *list.
- *@head:Pointer to a pointer that points to the head node.
- *@n:Element of the new node.
- *Return:Address of the new element, null if it fails.
+ * add_dnodeint_end - adds node to end of doubly linked list
+ * @head: pointer to head of list
+ * @n: integer value of node
+ * Return: pointer to node added
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = *head;
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *newNode;
+	dlistint_t *current = *head;
 
-	if (newNode == NULL)
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
 	{
+		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
 	newNode->n = n;
 	newNode->next = NULL;
-	newNode->prev = NULL;
 
-	if (temp == NULL)
+	if (!current)
 	{
+		newNode->prev = NULL;
 		*head = newNode;
 		return (newNode);
 	}
-	while (temp->next != NULL)
+
+	while (current->next)
 	{
-		temp = temp->next;
+		current = current->next;
 	}
-	temp->next = newNode;
-	newNode->prev = temp;
+	current->next = newNode;
+	newNode->prev = current;
 	return (newNode);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,68 +1,38 @@
 #include "lists.h"
 
 /**
- *insert_dnodeint_at_index - Inserts a new node at a given position.
- *@h:Pointer to a pointer that points to the head node.
- *@n: Data of the pointer to be inserted at index n.
- *@idx:Index of the list where the new node should be added.
- *Return: address of the new node.
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to head of list
+ * @idx: index to insert node
+ * @n: number of node added
+ * Return: node inserted
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	unsigned int len_of_d = sizeof_dlist(*h);
-	dlistint_t *temp = *h;
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h, *new;
 
-	if (idx > len_of_d + 1)
-		return (NULL);
-	if (newNode == NULL)
-	{
-		return (NULL);
-	}
-	newNode->n = n;
-	if (*h == NULL)
-	{
-		newNode->next = NULL;
-		newNode->prev = NULL;
-		*h = newNode;
-	}
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	else
+
+	for (; idx != 1; idx--)
 	{
-		for (i = 0; i < (idx - 1); i++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		newNode->next = temp->next;
-		newNode->prev = temp;
-		temp->next = newNode;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	return (newNode);
-}
 
-/**
- *sizeof_dlist - Finds the size of a doubly linked list.
- *@head: Pointer to the head of a doubly linked list.
- *Return: Size of the doubly linked list.
- */
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-int sizeof_dlist(dlistint_t *head)
-{
-	dlistint_t *temp = head;
-	int i = 0;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-	while (temp != NULL)
-	{
-		i++;
-		temp = temp->next;
-	}
-	return (i - 1);
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
