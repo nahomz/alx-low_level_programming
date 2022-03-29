83792f98cac98b92840c6411d78cefb5bb5d3a6c
nahomz <nahomzelalem11@gmail.com>
2022-03-29 18:48:01 +0000

    fix code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""
-    Technical interview preparation:
-"""
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """
-    returns the perimeter of the island described in grid:
-    grid is a list of list of integers:
-        0 represents a water zone
-        1 represents a land zone
-        One cell is a square with side length 1
-        Grid cells are connected horizontally/vertically (not diagonally).
-        Grid is rectangular, width and height don’t exceed 100
-    Grid is completely surrounded by water, and there is one
-    island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t
-    connected to the water around the island).
-    """
-    c = 0
-    for x in range(0, len(grid), 1):
-        for j in range(0, len(grid[0]), 1):
-            if grid[x][j] == 1:
-                c = c + 4
-                if j - 1 >= 0 and grid[x][j - 1] == 1:
-                    c -= 2
-                if x - 1 >= 0 and grid[x - 1][j] == 1:
-                    c -= 2
-    return (c)
+    """Returns the perimeter of the island in grid."""
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
+
+    return perim
